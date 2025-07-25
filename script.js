b4e20c62441a124aa73c572bf83ba7c909faf43f
AnthonyDavis2727 <107699398+AnthonyDavis2727@users.noreply.github.com>
2025-07-25 12:18:54 -0400

    Corrected the styling for the buttons. Fixed the styling and attributes for the new wur-com units that are added via button

--- script.js
+++ script.js
@@ -12,8 +12,15 @@ slider.oninput = function() {
 
 // Add another wurcom unit field set when button is clicked
 function addWurcomUnit() {
-    // Get the existing fieldset and create a new fieldset element
+    // Create a new fieldset element
     let newFieldset = document.createElement("fieldset");
+    newFieldset.className = "wurcom-units field-layout"
+
+    // Create new div elements and append to new fieldset
+    let newDiv1 = document.createElement("div");
+    newFieldset.appendChild(newDiv1);
+    let newDiv2 = document.createElement("div");
+    newFieldset.appendChild(newDiv2);
 
     // Get all existing fieldset elements and add one to the count
     let fieldsets = document.querySelectorAll("fieldset");
@@ -22,16 +29,20 @@ function addWurcomUnit() {
 
     // Create a label and input for the new unit name
     let nameLabel = document.createElement("label");
+    nameLabel.setAttribute("for", `wurcom_unit_${unitCount}`);
     nameLabel.textContent = `Wur-Com Name #${unitCount} *: `;
     let nameInput = document.createElement("input");
+    nameInput.id = `wurcom_unit_${unitCount}`;
     nameInput.type = "text";
     nameInput.name = `wurcom_unit_${unitCount}`;
     nameInput.required = true;
 
     // Create a label and input for the new unit serial number
     let serialLabel = document.createElement("label");
+    serialLabel.setAttribute("for", `wurcom_serial_${unitCount}`);
     serialLabel.textContent =  ` Wur-Com Serial Number #${unitCount} *: `;
     let serialInput = document.createElement("input");
+    serialInput.id = `wurcom_serial_${unitCount}`;
     serialInput.type = "text";
     serialInput.name = `wurcom_serial_${unitCount}`;
     serialInput.minLength = 8;
@@ -39,10 +50,10 @@ function addWurcomUnit() {
     serialInput.required = true;
 
     // Append the label and input to the fieldset
-    newFieldset.appendChild(nameLabel);
-    newFieldset.appendChild(nameInput);
-    newFieldset.appendChild(serialLabel);
-    newFieldset.appendChild(serialInput);
+    newDiv1.appendChild(nameLabel);
+    newDiv1.appendChild(nameInput);
+    newDiv2.appendChild(serialLabel);
+    newDiv2.appendChild(serialInput);
 
     // Append the new fieldset to the form
     lastFieldset.insertAdjacentElement('afterend', newFieldset);
